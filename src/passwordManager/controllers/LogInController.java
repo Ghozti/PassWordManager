5d70c89b488f8f14a434c7087fcc407aec90be14
Ghozti <ybalderas1005@gmail.com>
2021-03-09 16:24:45 -0500

    added fxml attributes and methods (also commnented)

--- src/passwordManager/controllers/LogInController.java
+++ src/passwordManager/controllers/LogInController.java
@@ -1,8 +1,21 @@
 package passwordManager.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LogInController {
+
+    //name text field found on the logIn UI
+    @FXML
+    private TextField nameField = new TextField();
+
+    //password text field found on the logIn UI
+    @FXML
+    private PasswordField passwordField = new PasswordField();
+
+    //once the logIn button id clicked this method will be called
     public void logIn(ActionEvent actionEvent) {
     }
 
