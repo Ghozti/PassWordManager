f45ca35d7bf473e553eede746ce5a0f7b4717062
Ghozti <ybalderas1005@gmail.com>
2021-03-10 09:02:54 -0500

    filled in FileCreator

--- src/passwordManager/Main.java
+++ src/passwordManager/Main.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import passwordManager.utils.FileCreator;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        FileCreator creator = new FileCreator();
+        creator.createFiles();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -19,5 +22,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void changeScene(String title, int w, int h, boolean resize){
+
     }
 }
--- src/passwordManager/utils/FileCreator.java
+++ src/passwordManager/utils/FileCreator.java
@@ -1,4 +1,40 @@
 package passwordManager.utils;
 
+import java.io.*;
+
 public class FileCreator {
+
+    public void createFiles() throws IOException {
+        String dir = createDirectory();
+        createName(dir);
+        createPassword(dir);
+    }
+
+    private String createDirectory(){
+        File directory = new File(System.getProperty("user.home") + "/SecureNotesDetails");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        return directory.getAbsolutePath();
+    }
+
+
+    private void createName(String path) throws IOException {
+        File directory = new File((path + "/name.txt"));
+        if (!directory.exists()) {
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(directory), "utf-8"))) {
+                writer.write("ghost");
+            }
+        }
+    }
+
+
+    private void createPassword(String path) throws IOException {
+        File directory = new File((path + "/password.txt"));
+        if (!directory.exists()) {
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(directory), "utf-8"))) {
+                writer.write("123");
+            }
+        }
+    }
 }
