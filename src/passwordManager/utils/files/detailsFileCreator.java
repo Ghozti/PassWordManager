55e5949ddf89484b79f3c6cf3680e0bf8adf5f30
Ghozti <ybalderas1005@gmail.com>
2021-03-11 16:34:36 -0500

    filled in detailsFileCreator

--- src/passwordManager/utils/files/detailsFileCreator.java
+++ src/passwordManager/utils/files/detailsFileCreator.java
@@ -1,18 +1,33 @@
 package passwordManager.utils.files;
 
-import java.io.File;
+import java.io.*;
 
 public class detailsFileCreator {
 
-    String createSubDir(String name) {
+    public String createSubDir(String name) {
         File directory = new File(System.getProperty("user.home") + "/PasswordManagerDetails/" + name);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
         return directory.getAbsolutePath();
     }
 
-    void createDirectory(String name) {
+    public String createAccountDir(String path, String name) {
+        File directory = new File(System.getProperty("user.home") + "/PasswordManagerDetails/" + path + "/" + name + "/");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        return directory.getAbsolutePath();
     }
 
-    void createFile(String name, String content) {
-
+    public void createFile(String path, String name, String content) {
+        File directory = new File((path + "/" + name + ".txt"));
+        if (!directory.exists()) {
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(directory), "utf-8"))) {
+                writer.write("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
